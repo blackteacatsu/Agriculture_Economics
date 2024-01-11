6ff7e85a9e5eebe80c5a84e08813d6d4471238e0
Daniel Yao <Daniel Yao>
2024-01-11 11:02:21 -0600

    Data Visualization

---
+++ Government Policies/Yao_Government_Policies.R
@@ -0,0 +1,8 @@
+# Effects of Government Policies on Algriculture
+
+data <- read.csv("C:/Users/yaoda/OneDrive/Desktop/JHU/2024 Intersession/EN.580.159.13 Introduction to Data Analysis and Visualization/Agriculture_Economics/Government Policies/FAOSTAT_data_en_1-11-2024.csv")
+
+class(data)
+dim(data)
+head(data)
+summary(data)
