16f5c2ccb9b75202bcc2de41daab23e5ab297553
Daniel Yao <Daniel Yao>
2024-01-12 11:12:42 -0600

    12 Jan 2024

--- Government Policies/Yao_Government_Policies.R
+++ Government Policies/Yao_Government_Policies.R
@@ -1,8 +1,33 @@
 # Effects of Government Policies on Algriculture
 
-data <- read.csv("C:/Users/yaoda/OneDrive/Desktop/JHU/2024 Intersession/EN.580.159.13 Introduction to Data Analysis and Visualization/Agriculture_Economics/Government Policies/2021_FAOSTAT_data_en_1-12-2024.csv")
+library(tidyverse)
 
-class(data)
-dim(data)
-head(data)
-summary(data)
+expenditures <- read.csv("C:/Users/yaoda/OneDrive/Desktop/JHU/2024 Intersession/EN.580.159.13 Introduction to Data Analysis and Visualization/Agriculture_Economics/Government Policies/2021_FAOSTAT_data_en_1-12-2024.csv")
+
+class(expenditures)
+dim(expenditures)
+head(expenditures)
+summary(expenditures)
+
+GDP_share <- read.csv("C:/Users/yaoda/OneDrive/Desktop/JHU/2024 Intersession/EN.580.159.13 Introduction to Data Analysis and Visualization/Agriculture_Economics/Government Policies/API_NV.AGR.TOTL.ZS_DS2_en_csv_v2_6299253.csv")
+
+class(GDP_share)
+dim(GDP_share)
+head(GDP_share)
+summary(GDP_share)
+
+GDP <- read.csv("C:/Users/yaoda/OneDrive/Desktop/JHU/2024 Intersession/EN.580.159.13 Introduction to Data Analysis and Visualization/Agriculture_Economics/Government Policies/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_6298258.csv")
+
+class(GDP)
+dim(GDP)
+head(GDP)
+summary(GDP)
+
+expenditure_values <- expenditures$"Value"
+GDP_values <- GDP$"X2021"
+
+expenditures_per_GDP <- expenditure_values / GDP_values
+
+expenditures_by_country <- expenditures %>% 
+  group_by(Area) %>% 
+  summarize(total_expenditures = sum(Value))
